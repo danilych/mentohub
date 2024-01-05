7746138d6967ed462984b0b9cce869b16e20b6c0
danilych <danilych45@proton.me>
2024-01-05 04:02:53 +0100

    feat: add typo and linking to footer

--- app/entities/footer.tsx
+++ app/entities/footer.tsx
@@ -1,67 +1,76 @@
 import {
   Facebook,
   Instagram,
-  Language,
   LogoMentohub,
-  Search,
   Telegram,
   Youtube,
 } from 'assets/icons'
 import { useTranslation } from 'react-i18next'
 import { Link } from '@remix-run/react'
 import useLocalizePathname from '~/i18n/use-localize-pathname'
-import { Dropdown } from 'flowbite-react'
-import useChangePathnameLocale from '~/i18n/use-change-pathname-locale'
-import { SupportedLocale } from '~/i18n/supported-locales'
 import Divider from '~/shared/divider'
 import { Text } from '~/shared/text'
 
-export let handle = { i18n: 'home' }
+export let handle = { i18n: 'common' }
 
 export default function Footer() {
-  let { t } = useTranslation('home')
+  let { t } = useTranslation('common')
   const localizePathname = useLocalizePathname()
-  const changePathnameLocale = useChangePathnameLocale()
 
   return (
-    <div className="flex gap-0 flex-col max-w-[1500px] mb-[42px] lg:px-[210px] md:px-[100px] px-[50px] min-h-[300px] bg-white font-semibold text-black">
+    <div className="flex gap-0 flex-col max-w-[1500px] mb-[42px] mx-auto min-mx-5 min-h-[300px] bg-white font-semibold text-black">
       <div className="flex relative flex-row gap-[218px] pb-[42px]">
         <div className="flex flex-col gap-[13px]">
           <p className="color-[#161616] text-lg leading-[25px] font-semibold not-italic font-manrope">
-            Навігація
+            {t('footer.navigation')}
           </p>
           <div className="flex flex-col gap-4">
-            <Text>Курси</Text>
-            <Text>Корпоративна освіта</Text>
-            <Text>Стати ментором</Text>
-            <Text>Мій кабінет</Text>
+            <Link to={localizePathname('#')}>
+              <Text>{t('footer.courses')}</Text>
+            </Link>
+            <Link to="#">
+              <Text>{t('footer.corporate-education')}</Text>
+            </Link>
+            <Link to="#">
+              <Text>{t('footer.become-mentor')}</Text>
+            </Link>
+            <Link to="#">
+              <Text>{t('footer.my-profile')}</Text>
+            </Link>
           </div>
         </div>
 
         <div className="flex flex-col gap-[13px]">
           <p className="color-[#161616] text-lg leading-[25px] font-semibold not-italic font-manrope">
-            Про нас
+            {t('footer.about-us')}
           </p>
           <div className="flex flex-col gap-4">
-            <Text>Контакти</Text>
-            <Text>Умови використання</Text>
-            <Text>Політика конфіденційності</Text>
-            <Text>Налаштування файлів cookie</Text>
+            <Link to="#">
+              <Text>{t('footer.contacts')}</Text>
+            </Link>
+            <Link to="#">
+              <Text>{t('footer.terms-of-use')}</Text>
+            </Link>
+            <Link to="#">
+              <Text>{t('footer.policy-of-privacy')}</Text>
+            </Link>
+            <Link to="#">
+              <Text>{t('footer.cookie-settings')}</Text>
+            </Link>
           </div>
         </div>
 
         <div className="right-0 absolute text-2xl flex flex-col gap-4">
           <Text>hello@mentohub.com</Text>
           <Text>+ 38 093 962 32 78</Text>
-          <Text>+38 068 467 38 92</Text>
+          <Text>+ 38 068 467 38 92</Text>
         </div>
       </div>
 
       <div className="relative">
-        
         <Divider />
         <div className="mt-6">
-          <div className='absolute left-0'>
+          <div className="absolute left-0">
             <Link to={localizePathname('/')}>
               <img
                 src={LogoMentohub}
@@ -70,7 +79,9 @@ export default function Footer() {
               />
             </Link>
 
-            <p className='text-sm font-normal leading-[19px]'>Copyright © 2023 Mentohub.</p>
+            <p className="text-sm font-normal leading-[19px]">
+              Copyright © 2023 Mentohub.
+            </p>
           </div>
 
           <div className="flex absolute right-0 flex-row gap-6">
