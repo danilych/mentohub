c8227b32069b55984897d9bfe2b74a4d436a8c34
danilych <danilych45@proton.me>
2024-01-24 21:31:28 +0100

    fix(cors): fix cors on server side

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,35 +1,119 @@
-import { RemixServer } from '@remix-run/react'
-import type { EntryContext } from '@remix-run/server-runtime'
-import { renderToString } from 'react-dom/server'
-import type { HandleDataRequestFunction } from '@remix-run/node'
+/**
+ * By default, Remix will handle generating the HTTP Response for you.
+ * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
+ * For more information, see https://remix.run/docs/en/main/file-conventions/entry.server
+ */
 
-export default async function handleRequest(
+import { PassThrough } from "node:stream";
+import type { EntryContext } from "@remix-run/node";
+import { Response } from "@remix-run/node";
+import { RemixServer } from "@remix-run/react";
+import isbot from "isbot";
+import { renderToPipeableStream } from "react-dom/server";
+
+const ABORT_DELAY = 5_000;
+
+export default function handleRequest(
   request: Request,
-  statusCode: number,
-  headers: Headers,
-  context: EntryContext
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
 ) {
-  const markup = renderToString(
-    <RemixServer context={context} url={request.url} />
-  )
-
-  headers.set('Content-Type', 'text/html')
-
-  return new Response('<!DOCTYPE html>' + markup, {
-    status: statusCode,
-    headers: headers,
-  })
+  return isbot(request.headers.get("user-agent"))
+    ? handleBotRequest(
+        request,
+        responseStatusCode,
+        responseHeaders,
+        remixContext
+      )
+    : handleBrowserRequest(
+        request,
+        responseStatusCode,
+        responseHeaders,
+        remixContext
+      );
 }
 
-// Set header Cache-Control for vercel
-export const handleDataRequest: HandleDataRequestFunction = (
-  response: Response
-) => {
-  if (!response.headers.get('Cache-Control')) {
-    response.headers.set(
-      'Cache-Control',
-      'max-age=0, s-maxage=2678400, stale-while-revalidate'
-    )
-  }
-  return response
+function handleBotRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
+) {
+  return new Promise((resolve, reject) => {
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer
+        context={remixContext}
+        url={request.url}
+        abortDelay={ABORT_DELAY}
+      />,
+      {
+        onAllReady() {
+          const body = new PassThrough();
+
+          responseHeaders.set("Content-Type", "text/html");
+
+          resolve(
+            new Response(body, {
+              headers: responseHeaders,
+              status: responseStatusCode,
+            })
+          );
+
+          pipe(body);
+        },
+        onShellError(error: unknown) {
+          reject(error);
+        },
+        onError(error: unknown) {
+          responseStatusCode = 500;
+          console.error(error);
+        },
+      }
+    );
+
+    setTimeout(abort, ABORT_DELAY);
+  });
+}
+
+function handleBrowserRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
+) {
+  return new Promise((resolve, reject) => {
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer
+        context={remixContext}
+        url={request.url}
+        abortDelay={ABORT_DELAY}
+      />,
+      {
+        onShellReady() {
+          const body = new PassThrough();
+
+          responseHeaders.set("Content-Type", "text/html");
+
+          resolve(
+            new Response(body, {
+              headers: responseHeaders,
+              status: responseStatusCode,
+            })
+          );
+
+          pipe(body);
+        },
+        onShellError(error: unknown) {
+          reject(error);
+        },
+        onError(error: unknown) {
+          console.error(error);
+          responseStatusCode = 500;
+        },
+      }
+    );
+
+    setTimeout(abort, ABORT_DELAY);
+  });
 }
