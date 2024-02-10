a35077be6de91d60afa1463949d3d8c4456cfe1c
danilych <danilych45@proton.me>
2024-02-10 21:14:54 +0100

    refactor(divider): add custom className in divider component

--- app/shared/divider.tsx
+++ app/shared/divider.tsx
@@ -1,7 +1,13 @@
-export function Divider() {
+import clsx from "clsx"
+
+interface Props {
+  className?: string
+}
+
+export function Divider({ className }: Props) {
   return (
     <div className="h-0">
-      <div className="h-0.5 bg-[#0F0F10] w-full"></div>
+      <div className={clsx("h-0.5 bg-[#0F0F10] w-full", className)}></div>
     </div>
   )
 }
