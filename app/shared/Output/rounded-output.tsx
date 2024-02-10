f4cdd663646034457feeb415f745c276e959bc28
danilych <danilych45@proton.me>
2024-02-10 21:16:35 +0100

    feat(component): implement rounded output component

---
+++ app/shared/Output/rounded-output.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx'
+import type { HTMLProps, ReactNode } from 'react'
+import React, { forwardRef, useImperativeHandle, useRef } from 'react'
+
+interface Props extends HTMLProps<HTMLInputElement> {
+  className?: string
+  children?: ReactNode
+}
+
+export function RoundedOutput({ className, children }: Props) {
+  return (
+    <div
+      className={clsx(
+        'text-[#4E4E51] py-[11px] h-[50px] bg-[#F6F6F6] rounded-[50px] outline-none text-sm font-manrope font-normal w-full border-0',
+        className
+      )}
+    >
+      <p className="ml-[18px] text-[#1f1f1f] text-base font-normal">{children}</p>
+    </div>
+  )
+}
